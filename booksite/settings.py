2486d1c665e717b7b9bb305bc69b93f9e9e40e65
HidekiTakazawa <ht0428a30@gmail.com>
2019-10-18 14:58:39 +0900

    heroku wsgi henkou

--- booksite/settings.py
+++ booksite/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # herokuのためここを追加
+    #'whitenoise.middleware.WhiteNoiseMiddleware', # herokuのためここを追加
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
